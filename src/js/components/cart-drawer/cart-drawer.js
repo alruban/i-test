1ffe4861d9bee3d74c536acd4e3ab638e9117a05
Sam Clarke <alruban@protonmail.com>
2025-10-29 23:21:58 +0000

    feat: more dialog js work

--- src/js/components/cart-drawer/cart-drawer.js
+++ src/js/components/cart-drawer/cart-drawer.js
@@ -4,19 +4,37 @@ export default class CartDrawer {
   constructor(element) {
     this.el = element;
 
-    this.exposeTriggers();
+    this.#exposeTriggers();
   }
 
-  exposeTriggers() {
-    this.elTriggers.forEach(trigger => {
-      trigger.addEventListener("click", () => {
-        if (this.el.open) {
-          this.el.close();
-        } else {
-          this.el.showModal();
+  #exposeTriggers = () => {
+    this.elTriggers.forEach(trigger => trigger.addEventListener("click", this.#handleAnimations));
+
+    this.el.addEventListener('click', ((e) => {
+      const rect = e.target.getBoundingClientRect();
+      if (rect.left > e.clientX ||
+            rect.right < event.clientX ||
+            rect.top > event.clientY ||
+            rect.bottom < event.clientY
+        ) {
+            this.#closeModal();
         }
-      });
-    });
+      })
+    );
+  }
+
+  #handleAnimations = () => {
+    if (this.el.open) {
+      this.#closeModal();
+    } else {
+      this.el.classList.remove("closing");
+      this.el.showModal();
+    }
+  }
+
+  #closeModal = () => {
+    this.el.addEventListener("animationend", () => setTimeout(() => this.el.close(), 300), { once: true });
+    this.el.classList.add("closing");
   }
 }
 
